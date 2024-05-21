fa006a4e233b77d901d4ad9354f5b53f6c8f065d
Caua Vaz <cauavaz1@gmail.com>
2024-05-21 20:57:53 -0300

    alteracao de commit

--- src/background.js
+++ src/background.js
@@ -3,7 +3,7 @@ function Background(context, canvasWidth, canvasHeight, jogador) {
     this.canvasWidth = canvasWidth;
     this.canvasHeight = canvasHeight;
     this.movSpeed = 0;
-
+tetstete
     this.images = [
         '../img/background/1.png',
         '../img/background/2.png',
